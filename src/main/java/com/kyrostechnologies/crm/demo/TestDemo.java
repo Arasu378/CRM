6b7bda4f23b012db0934bb465b78ce40c7893fd0
KyrosTechnologies <info@kyrostechnologies.com>
2017-11-16 19:18:56 +0530

    updated code some minor changes

---
+++ src/main/java/com/kyrostechnologies/crm/demo/TestDemo.java
@@ -0,0 +1,24 @@
+package com.kyrostechnologies.crm.demo;
+
+import java.util.List;
+
+public class TestDemo implements LanguageInterface{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<LanguageModel> getLanguageList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LanguageResponse InsertLanguage(String inParam1, String inParam2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
